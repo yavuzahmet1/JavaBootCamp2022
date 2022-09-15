062317866112ba0e6406ceacc20f3b540f04c4e2
Coders club <1yavuzahmet@gmail.com>
2022-09-15 23:21:30 +0300

    switch-case conditional

---
+++ switchDemo/src/switchDemo/Main.java
@@ -0,0 +1,26 @@
+package switchDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'C';
+
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+			System.out.println("Çok güzel : Geçtiniz");
+			break;
+		case 'C':
+			System.out.println("Fena deðil : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Maalesef : Kaldýnýz");
+			break;
+		default:
+			System.out.println("Geçersiz not aldýnýz");
+		}
+
+	}
+}
