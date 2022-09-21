1160e7a14743f3651fd5ab9c31ca18af798f163d
Coders club <1yavuzahmet@gmail.com>
2022-09-21 17:51:14 +0300

    friends numbers

---
+++ friendsNumbers/src/friendsNumbers/Main.java
@@ -0,0 +1,28 @@
+package friendsNumbers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 220;
+		int sayi2 = 289;
+		int toplam1 = 0;
+		int toplam2 = 0;
+
+		for (int i = 1; i < sayi1; i++) {
+			if (sayi1 % i == 0) {
+				toplam1 = toplam1 = i;
+			}
+		}
+		for (int i = 1; i < sayi2; i++) {
+			if (sayi2 % i == 0) {
+				toplam2 = toplam2 + i;
+			}
+		}
+		if (sayi1 == toplam2 && sayi2 == toplam2) {
+			System.out.println("Bu iki sayi arkaaþ sayýlardýr.");
+		} else {
+			System.out.println("Bu iki sayi arkadaþ sayý deðildir.");
+		}
+	}
+
+}
