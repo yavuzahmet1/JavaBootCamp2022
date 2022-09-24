60ddcfdaf495d286b7e81a7f635b6bf1f71af60e
Coders club <1yavuzahmet@gmail.com>
2022-09-24 23:32:40 +0300

    week 2 homework arrays

---
+++ week2OOP/week2HomeworkArrays/src/week2HomeworkArrays/Main.java
@@ -0,0 +1,13 @@
+package week2HomeworkArrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] country1 = new String[] { "Ankara", "Istanbul", "Adana", "Izmir" };
+
+		for (String s : country1) {
+			System.out.println(s);
+		}
+	}
+
+}
