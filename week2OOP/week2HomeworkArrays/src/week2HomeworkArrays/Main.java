a7c4b6a791fa82519d32e1b477f5d9b46de6edda
Coders club <1yavuzahmet@gmail.com>
2022-09-25 07:39:30 +0300

    week 2 homework arrays and methods

--- week2OOP/week2HomeworkArrays/src/week2HomeworkArrays/Main.java
+++ week2OOP/week2HomeworkArrays/src/week2HomeworkArrays/Main.java
@@ -3,11 +3,42 @@ package week2HomeworkArrays;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] country1 = new String[] { "Ankara", "Istanbul", "Adana", "Izmir" };
+		/*
+		 * String[] country1 = new String[] { "Ankara", "Istanbul", "Adana", "Izmir" };
+		 * 
+		 * for (String s : country1) { System.out.println(s); }
+		 */
 
-		for (String s : country1) {
-			System.out.println(s);
+		int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int searching = 45;
+
+		boolean isThere = numberSearch(numbers, searching);
+
+		getMessage(isThere, searching);
+	}
+
+	public static void getMessage(boolean isThere, int searching) {
+		String message = "";
+
+		if (isThere) {
+			message = "There is number : " + searching;
+			System.out.println(message);
+		} else {
+			message = ("There isn't number : " + searching);
+			System.out.println(message);
 		}
+	}
+
+	public static boolean numberSearch(int[] number, int searching) {
+		boolean isThere = false;
+
+		for (int num : number) {
+			if (num == searching) {
+				isThere = true;
+				break;
+			}
+		}
+		return isThere;
 	}
 
 }
