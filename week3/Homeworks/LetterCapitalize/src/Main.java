d0445945c2f0dcedda980e43bc40bb52e15bdf09
Coders club <1yavuzahmet@gmail.com>
2022-10-20 12:58:09 +0300

    Coderbyte LetterCapitalize

---
+++ week3/Homeworks/LetterCapitalize/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int[] x = new int[] { 1, 5, 10, 10 };
+		for (int i = 0; i < x[1] + 1; i++) {
+			System.out.println(i);
+			// for(int j=0;j<)
+		}
+
+	}
+
+}
