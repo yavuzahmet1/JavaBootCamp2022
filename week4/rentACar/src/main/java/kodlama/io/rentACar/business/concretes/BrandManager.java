da7babf75bacf668f58d193b88927375359eabff
Coders club <1yavuzahmet@gmail.com>
2022-10-26 00:24:01 +0300

    week 4 rent a car swagger entegrasyonu

--- week4/rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ week4/rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -2,13 +2,18 @@ package kodlama.io.rentACar.business.concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
+@Service // bu sınıf bir business nesnesidir.
 public class BrandManager implements BrandService {
 	private BrandRepository brandRepository_;
 
+	@Autowired
 	public BrandManager(BrandRepository brandRepository) {
 		brandRepository_ = brandRepository;
 	}
