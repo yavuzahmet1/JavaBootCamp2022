d3f65a7aeae34d9d6048eed2e92c7e47296864bd
Coders club <1yavuzahmet@gmail.com>
2022-11-14 22:34:48 +0300

    getbyid methot created

--- week5/Homework/dev-week5/dev-week5/src/main/java/com/kodlama/io/devweek5/service/concretes/SoftwareLanguageManager.java
+++ week5/Homework/dev-week5/dev-week5/src/main/java/com/kodlama/io/devweek5/service/concretes/SoftwareLanguageManager.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SoftwareLanguageManager implements SoftwareLanguageService {
@@ -48,6 +49,12 @@ public class SoftwareLanguageManager implements SoftwareLanguageService {
 
     @Override
     public SoftwareLanguageResponse getById(int id) throws Exception {
+        isValidId(id);
+        isNotExist(id);
+        Optional<SoftwareLanguage> softwareLanguage=this.softwareLanguageRepository.findById(id);
+        SoftwareLanguageResponse softwareLanguageResponse=new SoftwareLanguageResponse();
+        
+
         return null;
     }
 
