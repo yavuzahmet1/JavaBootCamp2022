fc5bd52fe2ab628a9fc7c601138bfd49678402a1
Coders club <1yavuzahmet@gmail.com>
2023-02-23 14:44:00 +0300

    week 6 spring boot version changed 2.7.5

--- week6/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ week6/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,10 +1,12 @@
 package kodlama.io.rentACar.entities.concretes;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 
 @Entity
